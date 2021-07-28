888815d19385a2fe141a6b3264389c5a1096fda9
DilshodIskandarov456 <76392416+DilshodIskandarov456@users.noreply.github.com>
2021-07-28 09:20:59 +0500

    PulBack New Version

---
+++ PulBack/SignInVC.swift
@@ -0,0 +1,14 @@
+//
+//  SignInVC.swift
+//  PulBack
+//
+//  Created by Dilshod Iskandarov on 7/28/21.
+import UIKit
+class SignInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        navigationController?.navigationBar.isHidden = true
+    }
+}
